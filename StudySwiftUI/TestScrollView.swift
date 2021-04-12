84084110150ef41bd3b76091ad507aea45bfe9b3
solchan87 <perudot@gmail.com>
2021-04-12 23:59:03 +0900

    Test

--- StudySwiftUI/TestScrollView.swift
+++ StudySwiftUI/TestScrollView.swift
@@ -16,7 +16,9 @@ struct TestScrollView: View {
       content: {
         VStack(spacing: 20) {
           ForEach(0..<100) { i in
-            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+            StackView {
+              Text("Test")
+            }
           }
         }
       })
